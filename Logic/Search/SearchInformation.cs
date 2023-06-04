240b80ffbdc760af8e71c0836558387efa16496b
Liam McGuire <liam.t.mcguire@gmail.com>
2023-06-03 21:37:14 -0400

    Added delegates for stopping a search which are used by the seperate thread that monitors the search time.

--- Logic/Search/SearchInformation.cs
+++ Logic/Search/SearchInformation.cs
@@ -38,6 +38,36 @@ namespace LTChess.Search
         /// </summary>
         public bool StopSearching = false;
 
+        /// <summary>
+        /// Set to true the first time that OnSearchFinish is invoked.
+        /// </summary>
+        public bool SearchFinishedCalled = false;
+
+        public void DoStopSearching()
+        {
+            StopSearching = true;
+        }
+
+        public void SetLastMove(Move move, int score)
+        {
+            if (!move.IsNull())
+            {
+                Log("SetLastMove(" + move + ", " + score + ") is replacing previous " + BestMove + ", " + BestScore);
+
+                this.BestMove = move;
+                this.BestScore = score;
+            }
+            else
+            {
+                //  This shouldn't happen.
+                Log("ERROR SetLastMove(" + move + ", " + score + ") " + "[old " + BestMove + ", " + BestScore + "] was illegal in FEN " + Position.GetFEN());
+            }
+        }
+
+        public void CallSearchFinish()
+        {
+            this.OnSearchFinish?.Invoke();
+        }
 
         /// <summary>
         /// A list of moves which the search thinks will be played next.
@@ -60,49 +90,70 @@ namespace LTChess.Search
         /// </summary>
         public ulong NodeCount = 0;
 
+        /// <summary>
+        /// Set to the value of wtime/btime if one was provided during a UCI "go" command.
+        /// If the search time gets too close to this, the search will stop prematurely so
+        /// we don't lose on time.
+        /// </summary>
+        public int PlayerTimeLeft = SearchConstants.MaxSearchTime;
+
         public SearchInformation(Position p, int depth)
         {
             this.Position = p;
             this.MaxDepth = depth;
-            StopSearching = false;
 
             PV = new Move[MAX_DEPTH];
 
             this.OnDepthFinish = () => Log(FormatSearchInformation(this));
         }
 
-        public SearchInformation(Position p, Action? onDepthFinish, int depth)
+        public SearchInformation(Position p, int depth, int searchTime)
         {
             this.Position = p;
             this.MaxDepth = depth;
-            StopSearching = false;
+            this.MaxSearchTime = searchTime;
 
             PV = new Move[MAX_DEPTH];
 
-            this.OnDepthFinish = onDepthFinish;
+            this.OnDepthFinish = () => Log(FormatSearchInformation(this));
         }
 
-        public string GetPVString()
+        public string GetPVString(bool EngineFormat = false)
         {
             StringBuilder pv = new StringBuilder();
             SimpleSearch.GetPV(this, this.PV, 0);
 
             Position temp = new Position(this.Position.GetFEN());
-            for (int i = 0; i < MAX_DEPTH; i++)
+            for (int i = 0; i < this.MaxDepth; i++)
             {
                 if (this.PV[i].IsNull())
                 {
                     break;
                 }
 
-                if (temp.bb.IsPseudoLegal(this.PV[i]))
+                if (EngineFormat)
                 {
-                    pv.Append(this.PV[i].ToString(temp) + " ");
-                    temp.MakeMove(this.PV[i]);
+                    if (temp.IsLegal(this.PV[i]))
+                    {
+                        pv.Append(this.PV[i] + " ");
+                        temp.MakeMove(this.PV[i]);
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
                 else
                 {
-                    pv.Append(this.PV[i].ToString() + "? ");
+                    if (temp.bb.IsPseudoLegal(this.PV[i]))
+                    {
+                        pv.Append(this.PV[i].ToString(temp) + " ");
+                        temp.MakeMove(this.PV[i]);
+                    }
+                    else
+                    {
+                        pv.Append(this.PV[i].ToString() + "? ");
+                    }
                 }
             }
 
@@ -113,7 +164,7 @@ namespace LTChess.Search
         {
             return "MaxDepth: " + MaxDepth + ", " + "MaxNodes: " + MaxNodes + ", " + "MaxSearchTime: " + MaxSearchTime + ", " 
                 + "BestMove: " + BestMove.ToString() + ", " + "BestScore: " + BestScore + ", " + "SearchTime: " + SearchTime + ", " 
-                + "NodeCount: " + NodeCount + ", " + "QNodeCount: " + SearchStatistics.QuiescenceNodes + ", " + "StopSearching: " + StopSearching;
+                + "NodeCount: " + NodeCount + ", " + "StopSearching: " + StopSearching;
         }
     }
 }
