4d776b19d83f16ef1f6bcee92d985e3f4118b4d3
Liam McGuire <liam.t.mcguire@gmail.com>
2023-06-15 17:10:56 -0400

    Reordered the scoring

--- Logic/Search/MoveScores.cs
+++ Logic/Search/MoveScores.cs
@@ -8,17 +8,15 @@ namespace LTChess.Search
 {
     public static class MoveScores
     {
-        //  Holy hell!
-        public const ushort EnPassant = 1660;
-
-        public const ushort PVMove = 10;
-        public const ushort TTHit = 9;
-        public const ushort WinningCapture = 8;
-        public const ushort EqualCapture = 7;
-        public const ushort KillerMove = 6;
-        public const ushort Check = 5;
-        public const ushort Castle = 4;
-        public const ushort Normal = 3;
-        public const ushort LosingCapture = 2;
+        //  https://www.chessprogramming.org/Move_Ordering
+        public const ushort PV_TT_Move = 100;
+        public const ushort WinningCapture = 90;
+        public const ushort EqualCapture = 80;
+        public const ushort KillerMove = 70;
+        public const ushort DoubleCheck = 65;
+        public const ushort Check = 60;
+        public const ushort Castle = 50;
+        public const ushort LosingCapture = 45;
+        public const ushort Normal = 40;
     }
 }
