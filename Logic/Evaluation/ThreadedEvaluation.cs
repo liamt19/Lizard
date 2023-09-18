4b1d4190d927f49dbb2a745249b76fddc6082500
Liam McGuire <liam.t.mcguire@gmail.com>
2023-09-18 15:28:44 -0400

    Reordered scores

--- Logic/Evaluation/ThreadedEvaluation.cs
+++ Logic/Evaluation/ThreadedEvaluation.cs
@@ -8,14 +8,18 @@ namespace LTChess.Logic.Search
     {
 
         public const short ScoreNone = 32760;
-        public const int ScoreMate = 30000;
         public const int ScoreInfinite = 31200;
+        public const int ScoreMate = 30000;
 
-        public const int ScoreMateMax = ScoreMate - MaxPly;
+        public const int ScoreTTWin = ScoreMate - 512;
+        public const int ScoreTTLoss = -ScoreTTWin;
+
+        public const int ScoreMateMax = ScoreMate - 256;
+        public const int ScoreMatedMax = -ScoreMateMax;
+
         public const int ScoreAssuredWin = 20000;
         public const int ScoreWin = 10000;
-        public const int ScoreTTWin = ScoreInfinite - 512;
-        public const int ScoreTTLoss = -ScoreTTWin;
+
         public const int ScoreDraw = 0;
 
         public const ulong EndgamePieces = 8;
