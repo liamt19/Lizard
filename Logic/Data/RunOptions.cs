6c6a4d36d610e066f5b2b17dfab27671967aae87
Liam McGuire <liam.t.mcguire@gmail.com>
2023-10-13 22:02:11 -0400

    SkipLocalsInit is now enabled by default

--- Logic/Data/RunOptions.cs
+++ Logic/Data/RunOptions.cs
@@ -6,14 +6,14 @@
 
 
 #define USE_SKIP_INIT
-#undef USE_SKIP_INIT
+//#undef USE_SKIP_INIT
 
 #if (USE_SKIP_INIT)
 
 //  Using this will cause methods to be generated without a ".locals init" flag,
 //  meaning that local variables aren't initialized to 0 when they are created.
 
-//  This does seem to make things run 2-3% faster, but this is actually dangerous
+//  Using SkipInit does seem to make things run 2-3% faster, but this can actually be dangerous
 //  since stackalloc'd arrays will have junk data in them in the indices that haven't been written to.
 [module: System.Runtime.CompilerServices.SkipLocalsInit]
 #endif
