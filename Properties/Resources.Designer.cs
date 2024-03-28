c8eb075595457ec64b9bce6219b196e627ae657e
Liam McGuire <liam.t.mcguire@gmail.com>
2024-03-28 17:58:44 -0400

    bench: 5423355

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,9 +63,9 @@ namespace Lizard.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
-        public static byte[] iguana_epoch10 {
+        public static byte[] lizard_1536x1x8_540 {
             get {
-                object obj = ResourceManager.GetObject("iguana-epoch10", resourceCulture);
+                object obj = ResourceManager.GetObject("lizard-1536x1x8-540", resourceCulture);
                 return ((byte[])(obj));
             }
         }
