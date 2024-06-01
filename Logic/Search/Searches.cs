81ddc1f17dca05695292b060c6abf2caa33fc6f5
Liam McGuire <12057628+liamt19@users.noreply.github.com>
2024-06-01 11:22:03 -0400

    Save a minimum depth of -1 in QSearch (#35)
    bench: 6342223

--- Logic/Search/Searches.cs
+++ Logic/Search/Searches.cs
@@ -752,7 +752,6 @@ namespace Lizard.Logic.Search
             }
 
             if (!isPV
-                && tte->Depth >= ttDepth
                 && ttScore != ScoreNone
                 && (tte->Bound & (ttScore >= beta ? BoundLower : BoundUpper)) != 0)
             {
@@ -924,7 +923,7 @@ namespace Lizard.Logic.Search
                       ((bestScore > startingAlpha) ? TTNodeType.Exact : 
                                                      TTNodeType.Beta);
 
-            tte->Update(pos.Hash, MakeTTScore((short)bestScore, ss->Ply), bound, depth, bestMove, ss->StaticEval, ss->TTPV);
+            tte->Update(pos.Hash, MakeTTScore((short)bestScore, ss->Ply), bound, ttDepth, bestMove, ss->StaticEval, ss->TTPV);
 
             return bestScore;
         }
