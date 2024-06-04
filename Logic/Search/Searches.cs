d86912c01c86b69280d31193234f9cc85135148c
Liam McGuire <liam.t.mcguire@gmail.com>
2024-06-03 20:47:00 -0400

    bench: 5795344

--- Logic/Search/Searches.cs
+++ Logic/Search/Searches.cs
@@ -236,7 +236,7 @@ namespace Lizard.Logic.Search
                 && (ss - 1)->CurrentMove != Move.Null
                 && pos.MaterialCountNonPawn[pos.ToMove] > 0)
             {
-                int reduction = NMPReductionBase + (depth / NMPReductionDivisor);
+                int reduction = 5 + (depth / 5) + Math.Min((eval - beta) / 173, 3);
                 ss->CurrentMove = Move.Null;
                 ss->ContinuationHistory = history.Continuations[0][0][0];
 
