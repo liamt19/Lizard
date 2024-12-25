c2b0b30ea7b6dd3b42ba3a5fb59bc4e80e0d8a9e
Liam McGuire <liam.t.mcguire@gmail.com>
2024-12-25 14:55:12 -0500

    bench: 4948914

--- Logic/Util/Utilities.cs
+++ Logic/Util/Utilities.cs
@@ -10,7 +10,7 @@ namespace Lizard.Logic.Util
 {
     public static class Utilities
     {
-        public const string EngineBuildVersion = "11.1.7";
+        public const string EngineBuildVersion = "11.1.8";
 
         public const int NormalListCapacity = 128;
         public const int MoveListSize = 256;
