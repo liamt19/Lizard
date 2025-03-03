81317043912618e0bbef3e2b12c6eb28ec160cdf
Liam McGuire <liam.t.mcguire@gmail.com>
2025-03-03 12:40:23 -0500

    bench: 4423310

--- Logic/Search/SearchOptions.cs
+++ Logic/Search/SearchOptions.cs
@@ -47,52 +47,52 @@
         public static int SEMinDepth = 5;
         public static int SENumerator = 11;
         public static int SEDoubleMargin = 24;
-        public static int SETripleMargin = 96;
-        public static int SETripleCapSub = 75;
+        public static int SETripleMargin = 97;
+        public static int SETripleCapSub = 73;
         public static int SEDepthAdj = -1;
 
         public static int NMPMinDepth = 6;
         public static int NMPBaseRed = 4;
         public static int NMPDepthDiv = 4;
-        public static int NMPEvalDiv = 181;
+        public static int NMPEvalDiv = 165;
         public static int NMPEvalMin = 2;
 
         public static int RFPMaxDepth = 6;
-        public static int RFPMargin = 47;
+        public static int RFPMargin = 46;
 
-        public static int ProbcutBeta = 256;
-        public static int ProbcutBetaImp = 101;
+        public static int ProbcutBeta = 257;
+        public static int ProbcutBetaImp = 93;
 
-        public static int NMFutileBase = 512;
-        public static int NMFutilePVCoeff = 1024;
-        public static int NMFutileImpCoeff = 1024;
-        public static int NMFutileHistCoeff = 1024;
-        public static int NMFutMarginB = 200;
-        public static int NMFutMarginM = 80;
-        public static int NMFutMarginDiv = 150;
+        public static int NMFutileBase = 473;
+        public static int NMFutilePVCoeff = 1062;
+        public static int NMFutileImpCoeff = 1017;
+        public static int NMFutileHistCoeff = 1051;
+        public static int NMFutMarginB = 180;
+        public static int NMFutMarginM = 81;
+        public static int NMFutMarginDiv = 139;
         public static int ShallowSEEMargin = 216;
         public static int ShallowMaxDepth = 9;
 
-        public static int LMRQuietDiv = 12288;
-        public static int LMRCaptureDiv = 10288;
-        public static int LMRExtMargin = 128;
+        public static int LMRQuietDiv = 12948;
+        public static int LMRCaptureDiv = 9424;
+        public static int LMRExtMargin = 132;
 
-        public static int QSFutileMargin = 183;
-        public static int QSSeeMargin = 81;
+        public static int QSFutileMargin = 187;
+        public static int QSSeeMargin = 78;
 
-        public static int OrderingCheckBonus = 9546;
-        public static int OrderingVictimMult = 12;
+        public static int OrderingCheckBonus = 9315;
+        public static int OrderingVictimMult = 11;
 
         public static int IIRMinDepth = 3;
         public static int AspWindow = 12;
 
-        public static int StatBonusMult = 184;
-        public static int StatBonusSub = 80;
-        public static int StatBonusMax = 1667;
+        public static int StatBonusMult = 182;
+        public static int StatBonusSub = 82;
+        public static int StatBonusMax = 1713;
 
-        public static int StatMalusMult = 611;
-        public static int StatMalusSub = 111;
-        public static int StatMalusMax = 1663;
+        public static int StatMalusMult = 654;
+        public static int StatMalusSub = 102;
+        public static int StatMalusMax = 1441;
 
         public static int SEEValuePawn = 105;
         public static int SEEValueKnight = 900;
